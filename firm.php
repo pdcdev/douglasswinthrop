e3a2f7d731c6530ff0d6edb6df497b25a6de38e2
slizano <sofia@lizano.org>
2015-06-11 13:13:05 -0400

    finished our firm

--- firm.php
+++ firm.php
@@ -8,19 +8,44 @@
             </div>
         </div>
     </aside>
-    <section class="history">
-        <h2 class="title"> History </h2>
-        <div class="description">
-            <p>Douglass Winthrop Advisors, LLC was founded in 2000 by Robert Douglass and Jay Winthrop to manage assets for high-net-worth individuals, 
-                families, trusts and endowments. From a modest beginning with only a few clients the firm now manages approximately $1 billion across more than 200 accounts. 
-                Our professional staff has grown to include Lea Paine Highet, a Certified Financial Planner with considerable wealth management expertise, 
-                as well as investment committee members Charlie Howard, Chars Crane, Bryce O’Brien, Grant Winthrop, Josh Huffard, Bowdy Train and Oak Strawbridge. 
-                Our professional team brings more than 150 years of collective investment experience to the daily rigors of overseeing our clients’ portfolios. 
-                Douglass Winthrop's success is due to its long-term record of growing and preserving clients' assets, its reputation for trust gained by always 
-                putting clients' interests first and its ability to attract talented partners. We believe this simple, elegant approach to business 
-                will continue to serve our clients and our firm well for many years to come. In April 2012 Douglass Winthrop moved into new headquarters 
-                at 535 Fifth Avenue to accommodate recent and future growth. We opened branch offices in Boston in late 2013 and in Washington, DC in early 2014.
-            </p>
+    <section class="firm">
+        <div>
+            <h2 class="title"> About </h2>
+            <div class="description">
+                <p class="sub-header">Douglass Winthrop is a New York City based SEC registered investment advisor. The firm manages more than $1.2 billion for individuals, families, trusts, and endowments. 
+                    Douglass Winthrop is owned and managed by its principals. 
+                </p>
+                <p class="following"> <br><br>We manage long-only equity and balanced accounts that are customized to the individual needs, objectives and risk tolerance of each client. We offer wealth management advice to clients when relevant. Our fee structure is simple and fair, keeping our goals aligned with those of our clients at all times. 
+                    Our primary objective is to grow and preserve our clients' capital.
+                </p>
+                
+            </div>
+        </div>
+        <div>
+            <h2 class="title"> Philosophy </h2>
+            <div class="description">
+                <p class="sub-header">We believe long term ownership of high quality stocks and bonds is the best strategy for compounding and preserving wealth. Our portfolios typically have 25 to 30 stocks with an average holding period of three to five years. 
+                    In balanced portfolios, we invest in high quality corporate, municipal and agency bonds.
+                </p>
+                <p class="following"><br><br>OThe principles of value investing underlie our approach to portfolio management. We own companies led by high integrity owner-managers, who run their firms for the benefit of shareholders. We select companies with near-permanent advantages arising from brand, industry leadership or economies of scale that increase the chances for success in times of stress. 
+                    Strong balance sheets back all of our equity and corporate bond holdings.
+                    <br><br>We invest relatively more capital behind our highest conviction ideas. Our approach aims to minimize the risk of capital loss while achieving superior results.
+                </p>
+            </div>
+        </div>
+        <div>
+            <h2 class="title"> History </h2>
+            <div class="description">
+                <p class="sub-header">Douglass Winthrop Advisors, LLC was founded in 2000 by Robert Douglass and Jay Winthrop to manage assets for high-net-worth individuals, families, trusts and endowments. 
+                </p>
+                <p class="following"> <br><br>From a modest beginning with only a few clients the firm now manages approximately $1 billion across more than 200 accounts. Our professional staff has grown to include Lea Paine Highet, 
+                    a Certified Financial Planner with considerable wealth management expertise, as well as investment committee members Charlie Howard, Chars Crane, Bryce O’Brien, Grant Winthrop, 
+                    Josh Huffard, Bowdy Train and Oak Strawbridge. Our professional team brings more than 150 years of collective investment experience to the daily rigors of overseeing our clients’ portfolios.
+                    Douglass Winthrop's success is due to its long-term record of growing and preserving clients' assets, its reputation for trust gained by always putting clients' interests first and its ability to attract 
+                    talented partners. We believe this simple, elegant approach to business will continue to serve our clients and our firm well for many years to come. 
+                    In April 2012 Douglass Winthrop moved into new headquarters at 535 Fifth Avenue to accommodate recent and future growth. We opened branch offices in Boston in late 2013 and in Washington, DC in early 2014.
+                </p>
+            </div>
         </div>
     </section>
 <?php include 'footer.php'; ?>
