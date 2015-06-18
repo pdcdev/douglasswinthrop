f884aab6a353747ddcb6f839ec06329dc778e272
slizano <sofia@lizano.org>
2015-06-18 17:32:32 -0400

    added banners to images folder

--- jay.php
+++ jay.php
@@ -1,138 +1,142 @@
 <?php include 'header.php'; ?>
-    <aside class="page_banner_people">
-        <!-- <div class="overlay">
-            <div class="inner_banner">
-                <div>
-                    <p>Our People</p>
-                </div>
-            </div>
-        </div> -->
+    <aside class="page_banner_jay">
+        
     </aside>
+    
     <section class="initial">
     <div>
-        <h2 class="people"> People </h2>
+        <h2 class="title-person-single">Jay Winthrop<br>Principal<br>robert@douglasswinthrop.com</h2>
+        <div class="description-person-single">
+            <p class="sub-header">
+            Jay is a principal in Douglass Winthrop Advisors LLC. Previously, Jay was a Vice President in the Mergers and Acquisitions group at JP Morgan Chase, where he provided financial and strategic advice to companies in a range of industries. Jay also worked at Hambrecht & Quist LLC, helping early stage companies raise capital in private and public markets. 
+            Jay has a lifelong interest in environmental policy and public service. He is a Vice Chairman of the Conservation Fund and former Chairman of American Farmland Trust. In 2014 Jay was appointed to the U.S. State Department’s Advisory Committee on International Economic Policy (ACIEP). Before co-founding Douglass Winthrop Advisors, Jay worked at the Office of Management and Budget and served two terms as an elected representative in his home town of Old Greenwich, Connecticut.
+            </p>
+        </div>
     </div>
     </section>
-    <section class="people">
-        <div class="person">
+
+
+    <section class="people-single">
+        <div class="person-single">
             <a href="robert.php"><img class="person_headshot" src="images/people/RobertDouglass_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <a href="robert.php"><h1>Robert Douglass</h1></a>
                 <p>Principal</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="jay.php"><img class="person_headshot" src="images/people/JayWinthrop_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="jay.php">Jay Winthrop</a></h1>
                 <p>Principal</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="rossana.php"><img class="person_headshot" src="images/people/RossanaAzzara_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="rossana.php">Rossana Azzara</a></h1>
                 <p>Client Relations Manager</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="cgcrane.php"><img class="person_headshot" src="images/people/CharlesCrane_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="cgcrane.php">Charles Crane</a></h1>
                 <p>Principal</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="sonny.php"><img class="person_headshot" src="images/people/SonnyDegady_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="sonny.php">Sonny Degady</a></h1>
                 <p>Vice President, Controller</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="lea.php"><img class="person_headshot" src="images/people/LeaPaineHighet_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="lea.php">Lea Paine Highet</a></h1>
                 <p>Principal</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="charlie.php"><img class="person_headshot" src="images/people/CharlesHoward_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="charlie.php">Charles Howard</a></h1>
                 <p>Principal</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="koren.php"><img class="person_headshot" src="images/people/Koren_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="koren.php">Koren Howe</a></h1>
                 <p>Client Relations Manager</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="josh.php"><img class="person_headshot" src="images/people/JoshHuffard_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="josh.php">Josh Huffard</a></h1>
                 <p>Principal</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="olivia.php"><img class="person_headshot" src="images/people/Olivia_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="olivia.php">Olivia Le Blan</a></h1>
                 <p>Associate</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="bryce.php"><img class="person_headshot" src="images/people/BryceO'Brien_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="bryce.php">D. Bryce O'Brien II</a></h1>
                 <p>Principal</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="alex.php"><img class="person_headshot" src="images/people/Alex_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="alex.php">Alexandra Panetta</a></h1>
                 <p>Client Administration Officer</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="oak.php"><img class="person_headshot" src="images/people/OakStrawbridge_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="oak.php">W. Oak Strawgridge</a></h1>
                 <p>Principal</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="bowdy.php"><img class="person_headshot" src="images/people/BowdyTrain_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="bowdy.php">C. Bowdoin Train</a></h1>
                 <p>Principal</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="lisa.php"><img class="person_headshot" src="images/people/LisaWade_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="lisa.php">Lisa Wade</a></h1>
                 <p>Client Relations Manager </p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="phil.php"><img class="person_headshot" src="images/people/Phil_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="phil.php">Phillip Warner</a></h1>
                 <p>Managing Director</p>
             </div>
         </div>
-        <div class="person">
+        <div class="person-single">
             <a href="grant.php"><img class="person_headshot" src="images/people/GrantWinthrop_Headshot.jpg"></a>
-            <div class="person_name">
+            <div class="person_name-single">
                 <h1><a href="grant.php">Grant Winthrop</a></h1>
                 <p>Managing Director</p>
             </div>
         </div>
 
     </section>
+   
 <?php include 'footer.php'; ?>
