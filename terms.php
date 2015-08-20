741c6effd65d83aedfa654a9037c27a6c4bdc466
slizano <sofia@lizano.org>
2015-08-20 13:15:39 -0400

    bold h2 to light to bold to lightmobile button active color to gray

--- terms.php
+++ terms.php
@@ -5,7 +5,7 @@
             <div class="inner_banner">
                 <div class="page-name">
                     <h1 class="terms">
-                        Credits / Legal
+                        Credits | Legal
                     </h1>
                 </div>
                 <div>
@@ -15,24 +15,26 @@
         </div><!-- end blue intro box -->
     </aside>
         <section class="terms">
-            <ul class="design-dev">
-                <li class="design-dev">Website Design and Development</li>
-                <li class="pdc">Piscatello Design Centre</li>
-            </ul>
+            
 
             <ul class="terms">       
                 <li class="legal">Legal Terms and Conditions</li>
                 <li>As of January 1, 2009, the Douglass Winthrop Advisors Composite (DWA Composite) includes all fee-paying accounts open for at least one quarter that provide data to Douglass Winthrop’s Advent system. Prior to January 1, 2009 DWA Composite included all fee-paying, discretionary accounts that provided data to Douglass Winthrop’s Advent system with an equity weighting of 65% or greater and a total value of $100,000 or greater on the last day of the prior quarter. DWA Equities represent the equity portion of all accounts in the DWA Composite. Results are calculated using a “Time Weighted Rate of Return” methodology, are gross of fees except where indicated, and include the reinvestment of dividends. Prior to January 1, 2009, DWA Composite excluded accounts that maintained legacy positions not initiated by Douglass Winthrop where these positions comprised more than 25% of account’s value, and accounts open for less than one quarter. Past performance is no guarantee of future performance, and any investment could lose value. Results are unaudited.
                 </li>       
             </ul>
+
+            <ul class="design-dev">
+                <li class="design-dev">Website Design and Development</li>
+                <li class="pdc">Piscatello Design Centre</li>
+            </ul>
         </section>
         
 <div id="footer">
-<div class="inner_footer">
-    <ul>
-        <li class="copyright">© 1997-2015 Douglass Winthrop  All rights reserved</li>
-        <li><a href ="terms.php">Credits / Legal</a></li>
-        <li><a href ="contact.php">Contact</a></li>
-    </ul>
-</div>
+    <div class="inner_footer">
+        <ul>
+            <li class="copyright">© 1997-2015 Douglass Winthrop  All rights reserved</li>
+            <li><a href ="terms.php">Legal   &nbsp;l&nbsp;   Credits</a></li>
+            <li><a href ="contact.php">Contact</a></li>
+        </ul>
+    </div>
 </div>
